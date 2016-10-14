5d00c06b072b7d9f7e0e9d98281a9add375f5387
DavisLien <DavisLien@163.com>
2016-10-14 19:10:50 +0800

    app.js 中实现了简单的路由分配；把实现路由功能的代码都放在 routes/index.js 里

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
+
+// 设置端口号
+app.set('port', process.env.PORT || 3000);
 // 设置 views 文件夹为存放视图文件的目录, 即存放模板文件的地方,
 // __dirname 为全局变量,存储当前正在执行的脚本所在的目录。
 app.set('views', path.join(__dirname, 'views'));
-
 // 设置视图模板引擎为 ejs
 app.set('view engine', 'ejs');
 
@@ -24,49 +26,51 @@ app.set('view engine', 'ejs');
 
 // 加载日志中间件
 app.use(logger('dev'));
-
 // 加载解析json的中间件
 app.use(bodyParser.json());
-
 // 加载解析urlencoded请求体的中间件
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // 加载解析cookie的中间件
 app.use(cookieParser());
-
 // 设置public文件夹为存放静态文件的目录
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 路由控制器
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
-// 捕获404错误，并转发到错误处理器
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-// 开发环境下的错误处理器，将错误信息渲染error模版并显示到浏览器中
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// // 路由控制器
+// app.use('/', routes);
+// app.use('/users', users);
 
-// 生产环境下的错误处理器，不会将错误信息泄露给用户
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// // 捕获404错误，并转发到错误处理器
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// 导出app实例供其他模块调用
-module.exports = app;
+// // 开发环境下的错误处理器，将错误信息渲染error模版并显示到浏览器中
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // 生产环境下的错误处理器，不会将错误信息泄露给用户
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
+
+// // 导出app实例供其他模块调用
+// module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 // 生成一个路由实例
 
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LPL' });
-});
+/**
+ * 这段代码的意思是当访问主页时，
+ * 调用 ejs 模板引擎，来渲染 index.ejs 模版文件
+ * (即将 title 变量全部替换为字符串 Express)，
+ * 生成静态页面并显示在浏览器中。
+ */
+ // router.get('/', function(req, res, next) {
+ //   res.render('index', { title: 'LPL' });
+ // });
 
-module.exports = router;
+ 
+module.exports = function(app) {
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: 'LPL' });
+  });
+};
