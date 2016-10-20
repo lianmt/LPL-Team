d4cecc6a3bf11268cdf32f0b6794ee62f8a235fa
lianmingtang <lianmingtang@163e.com>
2016-10-20 16:07:40 +0800

    存档功能

--- routes/index.js
+++ routes/index.js
@@ -235,7 +235,22 @@ module.exports = function(app) {
       });
     }); 
   });
-  
+  app.get('/archive', function (req, res) {
+    Post.getArchive(function (err, posts) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('/');
+      }
+      res.render('archive', {
+        title: '存档',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
   app.get('/u/:name/:day/:title', function (req, res) {
     Post.getOne(req.params.name, req.params.day, req.params.title, function (err, post) {
       if (err) {
